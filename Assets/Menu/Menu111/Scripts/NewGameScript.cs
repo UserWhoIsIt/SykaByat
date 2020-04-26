5cdebf181ca82b1914d64b26216cab66523776a3
UserWhoIsIt <vaniaes236@gmail.com>
2020-04-26 14:34:54 +0300

    SykaBlyat
    AAAAAAAAAAAAAAAAAAAAAAAAAnetblyat

---
+++ Assets/Menu/Menu111/Scripts/NewGameScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewGameScript : MonoBehaviour
+{
+
+    public Button yourButton;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene("scenename");
+    }
+
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+}
