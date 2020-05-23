dd255044b564f031b29d2c962ab4fe4ee7d6e523
c12games <57368063+c12games@users.noreply.github.com>
2020-05-23 18:09:29 +0300

    проізошла продуктивная работа

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.CodeDom;
+using System.Diagnostics;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,35 +12,84 @@ public class PlayerController : MonoBehaviour
     public float checkRadius;
     public float jumpTime;
     public Rigidbody2D rb;
-    private bool isGrounded;
+    public bool isGrounded;
     private float jumpTimeCounter;
-    private bool isJumping;
+    public bool isJumping;
+    public bool isRunning;
 
-    private bool isAttacking;
+    public bool isAttacking;
     public LayerMask whatIsGround;
 
     private Animator anim;
+    public AudioManager audioManager;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        audioManager = GetComponent<AudioManager>();
     }
 
     private void Update()
     {
+        Attack();
+        Jump();
+        Running();
+      /*  if (isJumping == true)
+        {
+            audioManager.Play("jump");
+        }
+        if(isAttacking == true)
+        {
+            audioManager.Play("attack1");
+            audioManager.Play("attack2");
+        }
+        if(isRunning == true)
+        {
+            audioManager.Play("running");
+        }  */
+    }
+    public void Running()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
+        if (moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+            
+        }
+
+        if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        
+    }
+    public void Attack()
+    {
         if (Input.GetKey(KeyCode.F))
         {
             anim.SetBool("isAttacking", true);
+            audioManager.Play("attack1");
+            audioManager.Play("attack2");
         }
         else
         {
             anim.SetBool("isAttacking", false);
         }
-
-
+    }
+   public void Jump()
+    {
         isGrounded = Physics2D.OverlapCircle(groundPos, checkRadius, whatIsGround);
 
         if (isGrounded == true && Input.GetKeyDown(KeyCode.W))
@@ -54,10 +104,13 @@ public class PlayerController : MonoBehaviour
             {
 
                 anim.SetBool("isJumping", false);
+
             }
             else
             {
                 anim.SetBool("isJumping", true);
+                audioManager.Play("jump");
+
             }
 
 
@@ -70,6 +123,7 @@ public class PlayerController : MonoBehaviour
                 rb.velocity = Vector2.up * jumpForce;
                 jumpTimeCounter -= Time.deltaTime;
                 anim.SetBool("isJumping", true);
+                audioManager.Play("jump");
             }
             else
             {
@@ -93,26 +147,9 @@ public class PlayerController : MonoBehaviour
             rb.velocity = Vector2.up * jumpForce;
         }
 
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        if (moveInput == 0)
-        {
-            anim.SetBool("isRunning", false);
-        }
-        else
-        {
-            anim.SetBool("isRunning", true);
-        }
 
-        if (moveInput < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-        else if (moveInput > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
+
 
 
 
@@ -120,4 +157,5 @@ public class PlayerController : MonoBehaviour
 
     }
 }
-
+ 
+     
